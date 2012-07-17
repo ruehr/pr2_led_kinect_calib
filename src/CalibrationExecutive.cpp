7117552d43a287f9ec24054b50c4327c1cc314b0
Thomas Ruehr <ruehr@in.tum.de>
2012-07-17 15:11:01 +0200

    now handles nans, checking for head to be settled instead of fixed time, take more evidence when things move in the background

--- src/CalibrationExecutive.cpp
+++ src/CalibrationExecutive.cpp
@@ -19,6 +19,7 @@
 std::string mount_frame_ = "head_mount_link";
 std::string rgb_optical_frame_ = "head_mount_kinect_ir_link";
 std::string rgb_topic_ = "/head_mount_kinect/depth_registered/points";
+std::string led_frame_ = "r_gripper_led_frame";
 
 tf::TransformListener*listener_ = 0L;
 
@@ -163,8 +164,10 @@ tf::Vector3 locate_led()
 
     tf::Vector3 best_pt;
 
+    double field_avg = 0;
+
     // trigger a few times until we have enough evidence we found the led
-    while (ros::ok() && (field_max<5000))
+    while (ros::ok() && ((field_max<2500) || (field_max < fabs(field_avg) * 100)))
     {
         if (waveform.request.active_low)
             waveform.request.active_low = 0;
@@ -186,6 +189,7 @@ tf::Vector3 locate_led()
         getCloud(cloud, rgb_optical_frame_, ros::Time::now(), &lookup_time);
 
         size_t max_index = 0;
+        field_avg = 0;
 
         if (have_last_cloud) {
             for (size_t k = 0; k < std::min(last_cloud->points.size(), cloud->points.size()); k++)
@@ -193,14 +197,18 @@ tf::Vector3 locate_led()
                 field[k] += ((cloud->points[k].r - last_cloud->points[k].r) +
                              (cloud->points[k].g - last_cloud->points[k].g) +
                              (cloud->points[k].b - last_cloud->points[k].b))  * sign;
+                field_avg += fabs(field[k]);
                 if (field[k]>field_max) {
                     field_max = field[k];
                     max_index = k;
                 }
             }
+            field_avg /= cloud->points.size();
             tf::Vector3 max_pt(cloud->points[max_index].x, cloud->points[max_index].y, cloud->points[max_index].z);
             best_pt = max_pt;
-            //std::cout << "max pt idx" << max_index << " (" << field_max << "):" << max_pt.x() << " " << max_pt.y() << " " << max_pt.z() << std::endl;
+            std::cout << "max pt idx" << max_index << " (" << field_max << "):" << "field abs avg " << field_avg << std::endl;
+            //max_pt.x() << " " << max_pt.y() << " " << max_pt.z() << std::endl;
+            //std::cout << "field avg " << field_avg << std::endl;
         } else {
             have_last_cloud = true;
         }
@@ -213,7 +221,7 @@ tf::Vector3 locate_led()
 void find_correspondance(tf::Vector3 &led_k, tf::Vector3 &led_tf)
 {
         led_k = locate_led();
-        tf::Stamped<tf::Pose> led_tf_pose = getPose(rgb_optical_frame_.c_str(),"r_gripper_led_frame");
+        tf::Stamped<tf::Pose> led_tf_pose = getPose(rgb_optical_frame_.c_str(),led_frame_.c_str());
         led_tf = led_tf_pose.getOrigin();
         if (!ros::ok())
             exit(0);
@@ -228,6 +236,9 @@ int main(int argc,char **argv)
 
     ros::init(argc, argv, "led_calibration_executive");
     ros::NodeHandle nh;
+
+    init();
+
     client = nh.serviceClient<ethercat_trigger_controllers::SetWaveform>("/r_gripper_led/set_waveform");
     //ethercat_trigger_controllers::SetWaveform waveform;
     waveform.request.rep_rate = 1;
@@ -261,25 +272,49 @@ int main(int argc,char **argv)
     {
         angle += 37.0 / 180.0 * M_PI;
         double radius = .2;
-        lookAt("r_gripper_led_frame", radius * sin(angle), radius * cos(angle),0);
+        lookAt(led_frame_.c_str(), radius * sin(angle), radius * cos(angle),0);
         //std::cout << angle << " " << radius * sin(angle) << " " <<  radius * cos(angle) << std::endl;
 
         // wait for the head turning to finish
-        ros::Duration(5).sleep();
+        //  ros::Duration(5).sleep();
+        ros::Rate rt(10);
+        ros::Time start = ros::Time::now();
+        tf::Stamped<tf::Pose> hand_pose = getPose(rgb_optical_frame_.c_str(),led_frame_.c_str());
+        tf::Stamped<tf::Pose> last_hand_pose;
+        double position_change = 5;
+        size_t num_settled = 0;
+        // wait for head vs hand tf to settle as the head should not move during calib
+        while  (num_settled < 3) {
+            rt.sleep();
+            last_hand_pose = hand_pose;
+            hand_pose = getPose(rgb_optical_frame_.c_str(),led_frame_.c_str());
+            //std::cout << "time " << (ros::Time::now() - start).toSec() << " pos " << (hand_pose.getOrigin() - last_hand_pose.getOrigin()).length() <<
+              //  " angle " << hand_pose.getRotation().angle(last_hand_pose.getRotation()) << std::endl;
+            position_change = (hand_pose.getOrigin() - last_hand_pose.getOrigin()).length();
+            if (position_change < 0.0001)
+                num_settled++;
+            else
+                num_settled = 0;
+        }
 
         tf::Vector3 led_tf, led_kinect;
-        find_correspondance(led_tf, led_kinect);
+        find_correspondance(led_kinect, led_tf);
         pcl::PointXYZ pt_k(led_kinect.x(),led_kinect.y(),led_kinect.z());
         pcl::PointXYZ pt_tf(led_tf.x(),led_tf.y(),led_tf.z());
-        points_in_kinect.points.push_back(pt_k);
-        points_in_tf.points.push_back(pt_tf);
+
+        if (led_kinect.x() != led_kinect.x()) {
+            ROS_WARN("Last point had nan position, skipping it");
+        } else {
+            points_in_kinect.points.push_back(pt_k);
+            points_in_tf.points.push_back(pt_tf);
+        }
 
         tf::Stamped<tf::Pose> kinect_pose = getPose(mount_frame_.c_str() ,rgb_optical_frame_.c_str());
         tf::Transform kinect_trans = kinect_pose;
 
         if (points_in_tf.points.size() > 2) {
             Eigen::Matrix4f trans;
-            transformation_estimator.estimateRigidTransformation(points_in_tf, points_in_kinect, trans);
+            transformation_estimator.estimateRigidTransformation(points_in_kinect,points_in_tf, trans);
 
             Eigen::Matrix4d md(trans.cast<double>());
             Eigen::Affine3d affine(md);
@@ -288,17 +323,19 @@ int main(int argc,char **argv)
 
             geometry_msgs::Transform ps;
             tf::transformTFToMsg(transform,ps);
-            std::cout << ps << std::endl;
+            std::cout << "Correction relative to current kinect pose:"<< std::endl;
+            std::cout << ps;
+            std::cout << "relative angle: " << transform.getRotation().getAngle() << " rad or " <<  transform.getRotation().getAngle() * 180.0f / M_PI << " degrees " << std::endl << std::endl;
 
             //geometry_msgs::Transform ps_kinect;
             //tf::transformTFToMsg(kinect_trans,ps_kinect);
             //std::cout << "kinect pose " << ps_kinect << std::endl;
 
             tf::Transform post = kinect_trans * transform;
-            tf::Transform pre = transform * kinect_trans ;
+            tf::Transform pre = transform * kinect_trans;
             geometry_msgs::Transform ps_post;
             tf::transformTFToMsg(post,ps_post);
-            std::cout << "kinect pose post " << ps_post << std::endl;
+            std::cout << "New pose of " <<  rgb_optical_frame_  << " in " << mount_frame_ << " \n " << ps_post << std::endl;
 
             btQuaternion q;
             double roll, pitch, yaw;
